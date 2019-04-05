39e51ab72ec0dfb622a70d7082ff98a897aa0b25
atomxSylvein <sylvein.faraux@atomxsystem.com>
2019-04-05 23:34:16 +0200

    import du module timesheet addon

---
+++ timesheet_addon/__manifest__.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "Timesheet Add-On",
+
+    'summary': """Allows to print timesheets""",
+
+    'description': """This module allows timesheets printing related to an employee""",
+
+    'author': "AtomX System",
+
+    'website': "http://atomxsystem.eu",
+
+    'category': 'Tools',
+
+    'version': '0.1',
+
+    # any module necessary for this one to work correctly
+    'depends': ['timesheet_grid'],
+
+    # always loaded
+    'data': [
+        'views/templates.xml',
+        'views/report.xml',
+    ],
+    'installable': True,
+    'auto_install': False,
+}
