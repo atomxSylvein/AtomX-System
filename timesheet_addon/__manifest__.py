79731724bdf3c7a79b36b0f10402cee202acfc0c
sfaraux <sylveinfaraux@hotmail.com>
2019-04-09 15:47:27 +0200

    id de l'action référré

--- timesheet_addon/__manifest__.py
+++ timesheet_addon/__manifest__.py
@@ -12,7 +12,7 @@
 
     'category': 'Tools',
 
-    'version': '0.2.16',
+    'version': '0.2.17',
 
     # any module necessary for this one to work correctly
     'depends': ['timesheet_grid', 'hr'],
