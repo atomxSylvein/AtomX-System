aa221c76441ddd2632f09c458917c8eb4e4e6659
sfaraux <sylveinfaraux@hotmail.com>
2019-04-09 19:19:29 +0200

    db

--- timesheet_addon/models/timesheetAddOn.py
+++ timesheet_addon/models/timesheetAddOn.py
@@ -34,13 +34,13 @@ class TimesheetAddOn(models.Model):
 		if rec.m_date_start:
 			domain.append(('date', '>=', rec.m_date_start))
 
-		if docs.m_date_end:
+		if rec.m_date_end:
 			domain.append(('date', '<=', rec.m_date_end))
 		"""if rec.m_date_start and rec.m_date_end:
 			domain = [('employee_id', '=', int(rec.m_employee)), ('date', '>=', rec.m_date_start),('date', '<=', rec.m_date_end)]
 		elif rec.m_date_start:
 			domain = [('employee_id', '=', int(rec.m_employee)), ('date', '>=', rec.m_date_start)]
-		elif docs.m_date_end:
+		elif rec.m_date_end:
 			domain = [('employee_id', '=', int(rec.m_employee)), ('date', '<=', rec.m_date_end)]
 		else:
 			domain = [('employee_id', '=', int(rec.m_employee))]"""
