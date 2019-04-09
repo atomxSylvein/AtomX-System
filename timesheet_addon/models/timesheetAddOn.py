1422aa0b67c345eafe63eeceae65bd310e86cd0d
sfaraux <sylveinfaraux@hotmail.com>
2019-04-09 17:50:48 +0200

    prise en compte de l'unité de mesure

--- timesheet_addon/models/timesheetAddOn.py
+++ timesheet_addon/models/timesheetAddOn.py
@@ -8,7 +8,10 @@ class TimesheetAddOn(models.Model):
 	m_employee = fields.Many2one('hr.employee', string="Employé", required=True)
 	m_date_start = fields.Date(string="Date de début", required=True)
 	m_date_end = fields.Date(string="Date de fin", required=True)
-	
+	m_config = fields.Many2one('res.config.settings', string="Configuration générale", readonly=True)
+	m_config_unit = fields.Many2one(related='m.config.timesheet_encode_uom_id', string="Encodage de la durée", readonly=True)
+	m_config_unit_name = fields.Char(related='m_config_unit.name', string="Unité de mesure", readonly=True)
+
 	@api.model
 	def print_timesheet(self, data):
 		"""Redirects to the report with the values obtained from the wizard
@@ -32,13 +35,13 @@ class TimesheetAddOn(models.Model):
 			domain = [('employee_id', '=', int(rec.m_employee))]
 
 		timesheets = timesheet_environment.search(domain, order='date asc')
-		
+
 		#Data recording
 		records = []
 		total = 0
 		for t in timesheets:
 			vals = {'project': t.account_id.name,
-					'duration': t.unit_amount,
+					'duration': t.unit_amount / t.product_uom_id.factor,
 					'task': t.task_id.name,
 					'description':t.name,
 					'date': t.date,}
@@ -46,7 +49,7 @@ class TimesheetAddOn(models.Model):
 			records.append(vals)
 
 		data['employee'] = rec.m_employee.name
-		data['unit'] = timesheets[0].product_uom_id.name
+		data['unit'] = rec.m_config_unit_name
 		data['timesheets'] = records
 		data['total'] = total
 
