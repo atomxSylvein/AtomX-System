331c35f1b90fd77d30bb68ae2952af0b64200d29
sfaraux <sylveinfaraux@hotmail.com>
2019-04-09 16:36:54 +0200

    transmission des données nécessaires

--- timesheet_addon/models/timesheetAddOn.py
+++ timesheet_addon/models/timesheetAddOn.py
@@ -8,24 +8,46 @@ class TimesheetAddOn(models.Model):
 	m_employee = fields.Many2one('hr.employee', string="Employé", required=True)
 	m_date_start = fields.Date(string="Date de début", required=True)
 	m_date_end = fields.Date(string="Date de fin", required=True)
-	m_timesheets = fields.Many2many('account.analytic.line', string="Feuilles de temps", readonly=True)
-
+	
 	@api.model
 	def print_timesheet(self, data):
 		"""Redirects to the report with the values obtained from the wizard
 		'data['form']': name of employee and the date duration"""
+
+		#Form reading
 		rec = self.browse(data)
 		data = {}
 		data['form'] = rec.read(['m_employee', 'm_date_start', 'm_date_end'])
-		#report_obj = self.env['report']
-		#report_doc = report_obj.get_action(self.browse(data), 'timesheets_by_employee.report_timesheets', data=data)
-		#report_doc = report_obj._get_report_from_name('timesheets_by_employee.report_timesheets')
+
+		#Query preparation using ORM
 		timesheet_environment = self.env['account.analytic.line']
 
-		domain = [('employee_id', '=', int(rec.m_employee))]
+		if rec.m_date_start and rec.m_date_end:
+			domain = [('employee_id', '=', int(rec.m_employee)), ('date', '>=', rec.m_date_start),('date', '<=', rec.m_date_end)]
+		elif rec.m_date_start:
+			domain = [('employee_id', '=', int(rec.m_employee)), ('date', '>=', rec.m_date_start)]
+		elif docs.m_date_end:
+			domain = [('employee_id', '=', int(rec.m_employee)), ('date', '<=', rec.m_date_end)]
+		else:
+			domain = [('employee_id', '=', int(rec.m_employee))]
 
-		data['timesheets'] = timesheet_environment.search(domain, order='date asc')
+		timesheets = timesheet_environment.search(domain, order='date asc')
 
-		#return self.env.ref('report.timesheet_addon.timesheet_report').report_action(self, data=data, config=False)
+		#Data recording
+		records = []
+		total = 0
+		for t in timesheets:
+			vals = {'project': t.account_id.name,
+					'duration': t.unit_amount,
+					'task': t.task_id.name,
+					'description':t.name,
+					'date': r.date,}
+			total += t.unit_amount
+			records.append(vals)
+
+		data['employee'] = rec.m_employee.name
+		data['unit'] = timesheets[0].product_uom_id.name
+		data['timesheets'] = records
+		data['total'] = total
+
 		return self.env.ref('timesheet_addon.action_timesheet_report').report_action(self, data=data, config=False)
-		#return self.env['report'].get_action(self.browse(data), 'timesheet_addon.timesheet_report', data=data)
