913771ecd0cf5f70f5102b5a5595d96d7e6c0a13
sfaraux <sylveinfaraux@hotmail.com>
2019-04-10 14:46:14 +0200

    db

--- timesheet_addon/__manifest__.py
+++ timesheet_addon/__manifest__.py
@@ -12,7 +12,7 @@
 
     'category': 'Tools',
 
-    'version': '0.2.42',
+    'version': '0.2.43',
 
     # any module necessary for this one to work correctly
     'depends': ['timesheet_grid', 'hr'],
