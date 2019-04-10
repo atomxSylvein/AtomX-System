407992f81a5c2d9e35b736b0ac357b27c04f065b
sfaraux <sylveinfaraux@hotmail.com>
2019-04-10 17:00:43 +0200

    report_name = "bobi.pdf"

--- timesheet_addon/__manifest__.py
+++ timesheet_addon/__manifest__.py
@@ -12,7 +12,7 @@
 
     'category': 'Tools',
 
-    'version': '0.2.56',
+    'version': '0.2.57',
 
     # any module necessary for this one to work correctly
     'depends': ['timesheet_grid', 'hr'],
