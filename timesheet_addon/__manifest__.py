bbfbfbc63a0ac44112c0bcf884b53dac0f05740d
sfaraux <sylveinfaraux@hotmail.com>
2019-04-10 16:58:30 +0200

    attachment use = false

--- timesheet_addon/__manifest__.py
+++ timesheet_addon/__manifest__.py
@@ -12,7 +12,7 @@
 
     'category': 'Tools',
 
-    'version': '0.2.55',
+    'version': '0.2.56',
 
     # any module necessary for this one to work correctly
     'depends': ['timesheet_grid', 'hr'],
