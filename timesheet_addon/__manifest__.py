058159b264a165f03dacc97333075b4c142da849
sfaraux <sylveinfaraux@hotmail.com>
2019-04-10 15:10:20 +0200

    retrait des tâches et leur description

--- timesheet_addon/__manifest__.py
+++ timesheet_addon/__manifest__.py
@@ -12,7 +12,7 @@
 
     'category': 'Tools',
 
-    'version': '0.2.44',
+    'version': '0.2.45',
 
     # any module necessary for this one to work correctly
     'depends': ['timesheet_grid', 'hr'],
