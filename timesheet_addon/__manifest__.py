d75265546c4ed3b3438d71c42a2d2facc85e2f3e
sfaraux <sylveinfaraux@hotmail.com>
2019-04-10 12:09:40 +0200

    db

--- timesheet_addon/__manifest__.py
+++ timesheet_addon/__manifest__.py
@@ -12,7 +12,7 @@
 
     'category': 'Tools',
 
-    'version': '0.2.36',
+    'version': '0.2.37',
 
     # any module necessary for this one to work correctly
     'depends': ['timesheet_grid', 'hr'],
--- timesheet_addon/models/timesheetAddOn.py
+++ timesheet_addon/models/timesheetAddOn.py
@@ -22,7 +22,7 @@ class TimesheetAddOn(models.Model):
 		rec = self.browse(data)
 		data = {}
 		data['form'] = rec.read(['m_employee', 'm_date_start', 'm_date_end', 'm_customer'])
-		data['form'][0]['m_date_start'] = datetime.strptime(data['form'][0]['m_date_start'], '%Y-%m-%d').strftime('%d/%m/%Y')
+		data['form'][0]['m_date_start'] = data['form'][0]['m_date_start'].strftime('%d/%m/%Y')
 		data['form'][0]['m_date_end'] = datetime.strptime(data['form'][0]['m_date_end'], '%Y-%m-%d').strftime('%d/%m/%Y')
 		#Query preparation using ORM
 		timesheet_environment = self.env['account.analytic.line']
