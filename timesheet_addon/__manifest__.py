54e3eec6c0b5d150f2fe5a29678af9789a5f8477
sfaraux <sylveinfaraux@hotmail.com>
2019-04-10 14:59:32 +0200

    ajustement

--- timesheet_addon/__manifest__.py
+++ timesheet_addon/__manifest__.py
@@ -12,7 +12,7 @@
 
     'category': 'Tools',
 
-    'version': '0.2.43',
+    'version': '0.2.44',
 
     # any module necessary for this one to work correctly
     'depends': ['timesheet_grid', 'hr'],
