94fec68efa3d973cc584c9d163599d87c15b6f64
sfaraux <sylveinfaraux@hotmail.com>
2019-04-10 15:28:19 +0200

    nom du report changeant

--- timesheet_addon/__manifest__.py
+++ timesheet_addon/__manifest__.py
@@ -12,7 +12,7 @@
 
     'category': 'Tools',
 
-    'version': '0.2.45',
+    'version': '0.2.46',
 
     # any module necessary for this one to work correctly
     'depends': ['timesheet_grid', 'hr'],
