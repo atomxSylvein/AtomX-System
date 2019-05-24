42626106829f878982a94b00de6a1fd51b40b0a2
SylveinFARAUX <sylvein.faraux@atomxsystem.com>
2019-05-24 17:16:36 +0200

    db

--- timesheet_addon/__manifest__.py
+++ timesheet_addon/__manifest__.py
@@ -12,7 +12,7 @@
 
     'category': 'Tools',
 
-    'version': '0.2.78',
+    'version': '0.2.79',
 
     # any module necessary for this one to work correctly
     'depends': ['timesheet_grid', 'hr'],
