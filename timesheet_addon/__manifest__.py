4c45308e08c06b9c4cce27f20296ddb49b170668
SylveinFARAUX <sylvein.faraux@atomxsystem.com>
2019-05-24 17:06:36 +0200

    m_customer -> partner_id

--- timesheet_addon/__manifest__.py
+++ timesheet_addon/__manifest__.py
@@ -12,7 +12,7 @@
 
     'category': 'Tools',
 
-    'version': '0.2.75',
+    'version': '0.2.76',
 
     # any module necessary for this one to work correctly
     'depends': ['timesheet_grid', 'hr'],
