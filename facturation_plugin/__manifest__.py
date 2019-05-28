68dcb66f16cd4fd3dde99fc68a42f7722ae7a98d
SylveinFARAUX <sylvein.faraux@atomxsystem.com>
2019-05-28 15:30:38 +0200

    reference modification (invoice report)

--- facturation_plugin/__manifest__.py
+++ facturation_plugin/__manifest__.py
@@ -12,7 +12,7 @@
 
     'category': 'Tools',
 
-    'version': '1.14',
+    'version': '1.15',
 
     # any module necessary for this one to work correctly
     'depends': ['account', 'sale', 'sale_management'],
