8d2b2aee0107fa3cacee99435c4ff43c4c8d3ec3
SylveinFARAUX <sylvein.faraux@atomxsystem.com>
2019-05-28 14:18:19 +0200

    db

--- facturation_plugin/__manifest__.py
+++ facturation_plugin/__manifest__.py
@@ -12,7 +12,7 @@
 
     'category': 'Tools',
 
-    'version': '1.10',
+    'version': '1.11',
 
     # any module necessary for this one to work correctly
     'depends': ['account', 'sale', 'sale_management'],
